d2de61a71c5b74e34deae8b34ec6655cd3259ed0
Amelie Rosser <amelierosser1986@gmail.com>
2017-08-16 13:55:56 +0100

    Cleaning up imageUtil

--- ARKitWeb/ImageUtil.swift
+++ ARKitWeb/ImageUtil.swift
@@ -16,11 +16,8 @@ class ImageUtil {
 
     var context: CIContext = CIContext()
     var colorSpace: CGColorSpace = CGColorSpace(name: CGColorSpace.sRGB)!
-//    var options: Dictionary<CFString, Any> = [ kCGImageDestinationLossyCompressionQuality: 0.5 ]
     let jpegCompressionQuality: CGFloat = 0.5
     let scale: CGFloat = 0.25;
-
-    // First attempt
 
     func getImageFromSampleBuffer (pixelBuffer: CVPixelBuffer) -> UIImage? {
         let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
@@ -36,22 +33,8 @@ class ImageUtil {
         let image = getImageFromSampleBuffer(pixelBuffer: pixelBuffer)
 
         if let base64String = UIImageJPEGRepresentation(image!, jpegCompressionQuality)?.base64EncodedString() {
-            // Upload base64String to your database
             return base64String;
         }
         return ""
     }
-
-    //    Second attempt - works but throws an error :/
-
-//    func getImageData(pixelBuffer: CVPixelBuffer) -> String {
-//        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-//
-//        // Scale image down by x percent
-//        let resizedCIImage = ciImage.transformed(by: CGAffineTransform(scaleX: 0.25, y: 0.25))
-//
-//        let jpegData = context.jpegRepresentation(of: resizedCIImage, colorSpace: colorSpace, options: options)
-//
-//        return (jpegData?.base64EncodedString())!
-//    }
 }
