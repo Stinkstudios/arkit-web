59c6136297f3cb3c5219aa23f084db70599938c6
Amelie Rosser <amelierosser1986@gmail.com>
2017-09-12 20:08:27 +0100

    Page is now changable on desktop

--- Demos/src/js/demos/gui.js
+++ Demos/src/js/demos/gui.js
@@ -1,5 +1,6 @@
 import dat from 'dat-gui';
 import ARKit from '../arkit/arkit';
+import { IS_NATIVE } from '../arkit/constants';
 
 const demos = ['anchors', 'bubbles', 'pointcloud', 'shadows'];
 
@@ -7,7 +8,7 @@ const path = window.location.pathname.split('/');
 let demo = path[path.length - 1].replace('.html', '').replace('/', '');
 
 // For index
-demo = demo === '' ? demos[0] : demo;
+demo = demo === '' || demo === 'index' ? demos[0] : demo;
 
 const controller = {
   demo
@@ -17,7 +18,12 @@ const gui = new dat.GUI();
 gui.open();
 
 gui.add(controller, 'demo', demos).onChange(demo => {
-  ARKit.loadPage(demo);
+  if (IS_NATIVE) {
+    ARKit.loadPage(demo);
+  } else {
+    demo = demo === demos[0] ? 'index' : demo;
+    window.location.href = `${demo}.html`;
+  }
 });
 
 export default gui;
