1efa471dbd07e58bc95dd773fdddabb79aed573f
Amelie Rosser <amelierosser1986@gmail.com>
2017-09-12 18:04:09 +0100

    Removing log
    Updating first demo name

--- Demos/src/js/demos/gui.js
+++ Demos/src/js/demos/gui.js
@@ -1,12 +1,10 @@
 import dat from 'dat-gui';
 import ARKit from '../arkit/arkit';
 
-const demos = ['index', 'bubbles', 'pointcloud', 'shadows'];
+const demos = ['anchors', 'bubbles', 'pointcloud', 'shadows'];
 
 const path = window.location.pathname.split('/');
 let demo = path[path.length - 1].replace('.html', '').replace('/', '');
-
-console.log(window.location.pathname);
 
 // For index
 demo = demo === '' ? demos[0] : demo;
